1d15a1b2ee3c5d80026325127c0eb8f79bcbd705
harshitkaushik444555 <harshitkaushik444555@gmail.com>
2024-10-23 10:40:53 +0530

    Create simple-shell-script.sh

---
+++ simple-shell-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello, World!"
+
+#Here first line tells the system that this script should be run using the bash shell.This is also called seabang. Shebang is a character sequence #! at the beginning of the script that tells the OS which interpreter to use to execute the script.
+#Second line just print the Hello World.
